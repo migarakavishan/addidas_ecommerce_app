078e6c687e4d8de52ab717ece38ef31348f1fc4b
migarakavishan <migarakavishan43@gmail.com>
2024-06-17 23:52:33 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/addidas_ecommerce_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.addidas_ecommerce_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/splash_img.png"),
+            const CupertinoActivityIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
